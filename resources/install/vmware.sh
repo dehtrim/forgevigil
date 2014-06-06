04a8e1ddc0eaf4eaf7c214491a0876aaf8e725ba
BlakeDarwinXe <BlakeDarwinXe@gmail.com>
2014-06-06 08:00:19 -0600

    Added install script for vmware tools

---
+++ resources/install/vmware.sh
@@ -0,0 +1,9 @@
+mkdir /mnt/cdrom
+mount /dev/cdrom /mnt/cdrom
+cp /mnt/cdrom/VMwareTools-*.tar.gz /tmp
+umount /mnt/cdrom
+tar -zxf /tmp/VMwareTools-*.tar.gz -C /tmp
+cd /
+./tmp/vmware-tools-distrib/vmware-install.pl --default
+rm -f /tmp/VMwareTools-*.tar.gz
+rm -rf /tmp/vmware-tools-distrib
